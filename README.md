c4e44c35cbb0549fe52b172966da8eb8f06876f1
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-08-04 08:33:10 -0700

    Initial commit. Add config files, README.md and general project structure to root directory.

---
+++ README.md
@@ -0,0 +1,45 @@
+# _Address Book_
+
+<!-- #### <a href="https://sonofakel.github.io/order-pizza/" target="_blank">Click here to view GitHub Page</a> -->
+
+#### _Web-based Address Book, August 4th, 2017_
+
+#### By _**Charlie Kelson**_
+
+## Specifications
+
+| Behavior  |  Input  | Output  |
+|---|---|---|
+|  |   |   |
+|  |   |   |  
+|  |   |   |  
+|  |   |   |  
+|  |   |   |  
+
+
+
+## Description
+
+_A website where you can store contact information for people you know.
+
+- Properties like the persons name, number and address are stored for later retrieval._
+
+## Setup/Installation Requirements
+
+* _Clone repo and open locally in modern browser._
+
+
+## Known Bugs
+
+_No known bugs at this time._
+
+
+
+## Technologies Used
+
+- C#
+- .NET
+- Razor
+
+
+Copyright (c) 2016 **_Charlie Kelson_**
---
+++ Startup.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AddressBook
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {   app.UseDeveloperExceptionPage();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
